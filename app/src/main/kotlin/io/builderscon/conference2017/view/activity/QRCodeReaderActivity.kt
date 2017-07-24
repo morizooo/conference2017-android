a2995bc2aeda3b4a887848b050d78106871fa756
morizooo <knnmrs999@gmail.com>
2017-07-24 20:32:27 +0900

    Add Activity

---
+++ app/src/main/kotlin/io/builderscon/conference2017/view/activity/QRCodeReaderActivity.kt
@@ -0,0 +1,13 @@
+package io.builderscon.conference2017.view.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.builderscon.conference2017.R
+
+class QRCodeReaderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode_reader)
+    }
+}
