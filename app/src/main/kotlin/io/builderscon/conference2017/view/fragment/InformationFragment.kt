444d04a2839988aac49e182e334c386037f822b7
morizooo <knnmrs999@gmail.com>
2017-07-27 03:11:06 +0900

    Update InformationFragment

--- app/src/main/kotlin/io/builderscon/conference2017/view/fragment/InformationFragment.kt
+++ app/src/main/kotlin/io/builderscon/conference2017/view/fragment/InformationFragment.kt
@@ -6,7 +6,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import io.builderscon.conference2017.R
+import kotlinx.android.synthetic.main.fragment_information.*
 
 
 class InformationFragment : Fragment() {
@@ -15,6 +19,18 @@ class InformationFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
 
         return inflater!!.inflate(R.layout.fragment_information, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+
+        val menu: MutableList<String> = mutableListOf("Sponsor", "FloorMap", "QRCodeReader", "License")
+        list_view.adapter =
+                ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, menu)
+        list_view.onItemClickListener = AdapterView.OnItemClickListener { parent, _, pos, _ ->
+            Toast.makeText(activity, parent.getItemAtPosition(pos) as String, Toast.LENGTH_SHORT).show()
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
