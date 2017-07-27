8b8d979e92a250835e4feb2dfadd933dd6206dca
morizooo <knnmrs999@gmail.com>
2017-07-27 21:48:34 +0900

    convert DataBindingHelper

---
+++ app/src/main/kotlin/io/builderscon/conference2017/view/DataBindingHelper.kt
@@ -0,0 +1,48 @@
+package io.builderscon.conference2017.view
+
+import android.databinding.BindingAdapter
+import android.support.annotation.DrawableRes
+import android.view.View
+
+import com.squareup.picasso.Picasso
+
+import org.lucasr.twowayview.widget.SpannableGridLayoutManager
+
+import de.hdodenhof.circleimageview.CircleImageView
+
+object DataBindingHelper {
+
+    @BindingAdapter("imageUrl")
+    @JvmStatic
+    fun setImageUrl(view: CircleImageView, imageUrl: String) {
+        if (!imageUrl.isEmpty()) {
+            Picasso.with(view.context)
+                    .load(imageUrl)
+                    .into(view)
+        }
+
+    }
+
+    @BindingAdapter("sessionCellBackground")
+    @JvmStatic
+    fun setSessionCellBackground(view: View, @DrawableRes backgroundResId: Int) {
+        view.setBackgroundResource(backgroundResId)
+    }
+
+    @BindingAdapter("twowayview_rowSpan")
+    @JvmStatic
+    fun setTwowayViewRowSpan(view: View, rowSpan: Int) {
+        val lp = view.layoutParams as SpannableGridLayoutManager.LayoutParams
+        lp.rowSpan = rowSpan
+        view.layoutParams = lp
+    }
+
+    @BindingAdapter("twowayview_colSpan")
+    @JvmStatic
+    fun setTwowayViewColSpan(view: View, colSpan: Int) {
+        val lp = view.layoutParams as SpannableGridLayoutManager.LayoutParams
+        lp.colSpan = colSpan
+        view.layoutParams = lp
+    }
+
+}
