66b01b32a1efed29a87a3228d38c0d6724bf8ca7
y_morioka <y_morioka@septeni-original.co.jp>
2017-07-30 19:15:09 +0900

    Fix Timezone

---
+++ app/src/main/kotlin/io/builderscon/conference2017/MainApplication.kt
@@ -0,0 +1,9 @@
+package io.builderscon.conference2017
+
+import android.support.multidex.MultiDexApplication
+
+class MainApplication: MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/kotlin/io/builderscon/conference2017/extension/DateExtension.kt
+++ app/src/main/kotlin/io/builderscon/conference2017/extension/DateExtension.kt
@@ -1,42 +1,43 @@
 package io.builderscon.conference2017.extension
 
-import khronos.toDate
-import khronos.toString
+import java.text.SimpleDateFormat
 import java.util.*
 
+private fun  getJstFormat(format:String) : SimpleDateFormat= SimpleDateFormat(format).apply {
+    timeZone = TimeZone.getTimeZone("Asia/Tokyo")
+}
+
 fun Date.getHourMinute(): String {
-    return this.toString("kk:mm")
+    return getJstFormat("kk:mm").format(this)
 }
 
 fun Date.getFormatDate(): String {
-    return this.toString("yyyyMMdd")
+    return getJstFormat("yyyyMMdd").format(this)
 }
 
 fun Date.getLongFormatDate(): String {
-    return this.toString("yyyy/MM/dd/ kk:mm")
+    return getJstFormat("yyyy/MM/dd kk:mm").format(this)
 }
 
 fun Date.toMD(): String {
-    return this.toString("M/d")
+    return getJstFormat("M/d").format(this)
 }
 
 // for display
-val closingDate = "2017-08-05 16:50".toDate("yyyy-MM-dd hh:mm")
-val ignoreSameTimes = listOf("2017-08-04 11:30".toDate("yyyy-MM-dd hh:mm")
-        , "2017-08-05 15:50".toDate("yyyy-MM-dd hh:mm"))
-val ignoreTmpTimes = listOf("2017-08-04 11:00".toDate("yyyy-MM-dd hh:mm")
-        , "2017-08-05 15:20".toDate("yyyy-MM-dd hh:mm"))
+val closingDate = "2017/08/05 16:50"
+val ignoreSameTimes = listOf("2017/08/04 11:30", "2017/08/05 15:50")
+val ignoreTmpTimes = listOf("2017/08/04 11:00", "2017/08/05 15:20")
 
 fun Date.needAddEmptyTime(): Boolean {
-    return this == closingDate
+    return this.getLongFormatDate() == closingDate
 }
 
 fun Date.ignoreSameTimes(): Boolean {
-    return ignoreSameTimes.contains(this)
+    return ignoreSameTimes.contains(this.getLongFormatDate())
 }
 
 fun Date.ignoreTmpTimes(): Boolean {
-    return ignoreTmpTimes.contains(this)
+    return ignoreTmpTimes.contains(this.getLongFormatDate())
 }
 
 fun Date.needsAdjustColSpan(): Boolean {
